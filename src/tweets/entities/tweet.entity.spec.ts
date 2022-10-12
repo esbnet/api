3d747e35402e0b70a472313f2836e70d49ddccbb
Edmilson Soares <esbnet@gmail.com>
2022-10-12 20:40:26 -0300

    create test entity tweet

--- src/tweets/entities/tweet.entity.spec.ts
+++ src/tweets/entities/tweet.entity.spec.ts
@@ -1,12 +1,45 @@
-import { Tweet } from './tweet.entity';
+import mongoose from 'mongoose';
+import { Tweet, TweetSchema } from './tweet.entity';
+
 
 describe('Tweet Tests', () => {
-  it('should be able to create a new Tweet', () => {
-    const tweet = new Tweet({
-      content: 'Test Hello World!',
-      screen_name: 'John',
+  describe('Tweet Class', () => {
+    it('should be able to create a new Tweet', () => {
+      const tweet = new Tweet({
+        content: 'Test Hello World!',
+        screen_name: 'John',
+      });
+      expect(tweet.content).toBe('Test Hello World!');
+      expect(tweet.screen_name).toBe('John');
     });
-    expect(tweet.content).toBe('Test Hello World!');
-    expect(tweet.screen_name).toBe('John');
   });
+
+  describe('Tweet Persistence', () => {
+    let conn: mongoose.Mongoose;
+
+    beforeEach(async () => {
+      conn = await mongoose.connect('mongodb://root:root@localhost:27017/tweets_test?authSource=admin')
+    })
+
+    afterEach(async () => {
+      await conn.disconnect();
+    })
+
+    it('create a tweet document ok!', async () => {
+
+      const TweetModel = conn.model('Tweet', TweetSchema)
+
+      const tweet = new TweetModel({
+        content: 'Test Hello World!',
+        screen_name: 'John',
+      });
+
+      await tweet.save();
+      const tweetCreated = await TweetModel.findById(tweet._id)
+
+      expect(tweetCreated.content).toBe('Test Hello World!');
+      expect(tweetCreated.screen_name).toBe('John');
+
+    })
+  })
 });
--- src/tweets/entities/tweet.entity.ts
+++ src/tweets/entities/tweet.entity.ts
@@ -1,14 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
 export type TweetProps = {
   content: string;
   screen_name: string;
 };
 
+@Schema()
 export class Tweet {
+
+  @Prop({ required: true })
   content: string;
+
+  @Prop({ required: true })
   screen_name: string;
 
   constructor(props: TweetProps) {
     this.content = props.content;
     this.screen_name = props.screen_name;
   }
+
 }
+
+export const TweetSchema = SchemaFactory.createForClass(Tweet)
